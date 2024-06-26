b4e08e763dc96bc9658b63b67a5b2f5c61f90eb3
nqthang21 <nqthang21@gmail.com>
2024-06-26 10:52:16 +0700

    reusable widget

--- lib/gradient_contailer.dart
+++ lib/gradient_contailer.dart
@@ -5,16 +5,19 @@ const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            Colors.deepPurple,
-            Colors.pink,
+            color1,
+            color2,
           ],
           begin: startAlignment,
           end: endAlignment,
@@ -26,3 +29,26 @@ class GradientContainer extends StatelessWidget {
     );
   }
 }
+
+
+// class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(
+//         child: StyledText('Hello World 123'),
+//       ),
+//     );
+//   }
+// }
